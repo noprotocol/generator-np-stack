db95648362c6a0cba34e586af285556f36102ade
Hyra <stef@mindthecode.com>
2018-03-12 17:34:54 +0100

    Comments 😘

--- generators/app/laravel/install.js
+++ generators/app/laravel/install.js
@@ -3,6 +3,7 @@ const fs = require("fs-extra");
 module.exports = function(generator) {
   generator.log("✏️  Generating Laravel app");
 
+  // Install Laravel through composer
   generator.spawnCommandSync("composer", [
     "create-project",
     "--prefer-dist",
@@ -10,7 +11,9 @@ module.exports = function(generator) {
     generator.laravelInstallPath
   ]);
 
+  // Remove the generated package.json
   fs.removeSync("_backend/package.json");
 
+  // Remove the generated .gitignore file
   fs.removeSync("_backend/.gitignore");
 };
