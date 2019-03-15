7204088bb9558f634d6d1f238a1f3eca03ab54f4
Bob Fanger <bfanger@gmail.com>
2019-03-15 16:24:05 +0100

    Implemented Hot Module Reloading for Vuex
    Fixes:
    TypeError: Cannot read property 'extend' of undefined
    Something went wrong during Vue component hot-reload. Full reload required.

--- generators/app/templates/vue/src/store/index.js
+++ generators/app/templates/vue/src/store/index.js
@@ -8,22 +8,37 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
-const requireModule = require.context(".", false, /\.js$/);
 
-const modules = {};
-for (const filename of requireModule.keys()) {
-  if (filename === "./index.js") {
-    continue;
+const refresh = false; // When modules change: do a full-page-refresh instead of a hotUpdate.
+
+function importModules() {
+  const requireModule = require.context(".", false, /\.js$/);
+  const modules = {};
+  for (const filename of requireModule.keys()) {
+    if (filename === "./index.js") {
+      continue;
+    }
+    const name = filename.replace(/^\.\/|\.js$/g, "");
+    modules[name] = {
+      strict: true,
+      namespaced: true,
+      ...requireModule(filename)
+    };
   }
-  const name = filename.replace(/^\.\/|\.js$/g, "");
-  modules[name] = {
-    strict: true,
-    namespaced: true,
-    ...requireModule(filename)
-  };
+  if (module.hot) {
+    if (refresh) {
+      module.hot.decline(requireModule.id);
+    } else {
+      module.hot.accept(requireModule.id, () => {
+        store.hotUpdate({ modules: importModules() });
+      });
+    }
+  }
+  return modules;
 }
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   strict: true,
-  modules
+  modules: importModules()
 });
+export default store;
